5cd0c546ac67c548e74c3ea0bdbcce49800c2dfa
Josh Sharp <josh@joshsharp.com.au>
2017-09-21 21:38:35 +1000

    tests work

--- src/braid/main.go
+++ src/braid/main.go
@@ -4,92 +4,11 @@ import (
 	"fmt"
 	"strings"
 	"braid/ast"
+	"os"
+	"io/ioutil"
 )
 
-func main() {
-	examples := []string{`
-# test
-let cheesy = func item item2 {
-	let b = 5.0 +. 6.5
-	let c = [5, 6, 7]
-    # more test
-    item ++ " and " ++ item2 ++ " with cheese"
-}
-
-let tester = func a {
-	let nothing = a + 1
-	if a > 100 {
-		a + 1
-	} else if a > 5 {
-		a + 50
-	} else {
-		a + 100
-	}
-}
-
-let main = func {
-	let something = func {
-		4 + 9
-	}
-	let result = 5 * (4 + 6) * 2
-	let yumPizza = cheesy("pineapple", ("bbq" ++ "sauce"))
-	# hoo boy this is a good'un
-	let five = 1 / 1 + 3 * (55 - 2)
-	# let mmm = 1 + 1
-}
-`, `
-let main = func {
-	# one
-	let a = 2
-	# two
-	let b = 3 + -2
-	let c = a + b
-	let d = [5, 6]
-	let e = b
-	let _ = List.add(1.0, 2.0, [3.0])
-	adder(4, 5)
-	Mod.f()
-}
-`,
-`
-type Person = { name: string, age: int }
-
-type IntList = list int
-
-type Result 'a 'b =
-	| OK 'a
-	| Error 'b
-
-type Option 'a =
-	| Some 'a
-	| None
-
-let main = func {
-	# thing
-	let a = 3
-	let b = 45
-	let c = 5
-	# no
-	let d = [5, 6]
-	let e = b
-	test((5 + 6), Person{name: "no", age: -1})
-}
-
-let test = func p {
-	let c = 5 + 5
-	# mm
-	let a = Person{name:"Josh", age: 32}
-	let b = OK("yes")
-	let c = Error("failed to do thing")
-	let d = Some("braid")
-	let e = None()
-	# hi
-}
-
-`}
-
-	input := examples[0]
-
+func Compile(input string) (string, error) {
 	lines := strings.Split(input, "\n")
 
 	// first we make a reader from the input, which is a string
@@ -107,6 +26,7 @@ let test = func p {
 			pe := err.(ast.ParserError)
 			printError(pe, lines)
 		}
+		return "", err
 	} else {
 		// print the input
 		for i, el := range lines {
@@ -123,16 +43,19 @@ let test = func p {
 		_, err := ast.Infer(a.(ast.Module), &env, nil)
 		if err != nil {
 			fmt.Println(err.Error())
-			return
+			return "", err
 		}
 
 		fmt.Println(env)
 
 		// print the compiled Go
-		fmt.Println(a.Compile(env))
+		result := a.Compile(env)
+		fmt.Println(result)
+		return result, nil
 	}
 
 }
+
 
 func printError(pe ast.ParserError, lines []string) {
 
@@ -164,4 +87,28 @@ func printError(pe ast.ParserError, lines []string) {
 	// print the actual error
 	fmt.Printf("Line %d, character %d: ", pe.Pos()[0], pe.Pos()[1])
 	fmt.Println(pe.InnerError())
+}
+
+
+func main() {
+	args := os.Args[1:]
+
+	if len(args) == 0 {
+		fmt.Printf("Must supply an argument of a file to compile, eg. `$ braid example.bd`\n")
+		return
+	}
+
+	result, err := ioutil.ReadFile(args[0])
+	if err != nil {
+		fmt.Printf("Error reading file %s: %s\n", args[0], err.Error())
+		return
+	}
+
+	file := string(result)
+	compiled, err := Compile(file)
+	if err != nil {
+		return
+	}
+	fmt.Print(compiled)
+
 }
---
+++ src/braid/main_test.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"testing"
+)
+
+var examples = []string{`
+# test
+let cheesy = func item item2 {
+	let b = 5.0 +. 6.5
+	let c = [5, 6, 7]
+    # more test
+    item ++ " and " ++ item2 ++ " with cheese"
+}
+
+let tester = func a {
+	let nothing = a + 1
+	if a > 100 {
+		a + 1
+	} else if a > 5 {
+		a + 50
+	} else {
+		a + 100
+	}
+}
+
+let main = func {
+	let something = func {
+		4 + 9
+	}
+	let result = 5 * (4 + 6) * 2
+	let yumPizza = cheesy("pineapple", ("bbq" ++ "sauce"))
+	# hoo boy this is a good'un
+	let five = 1 / 1 + 3 * (55 - 2)
+	# let mmm = 1 + 1
+}
+`, `
+
+let add = func a b {
+	a + b
+}
+
+let main = func {
+	# one
+	let a = 2
+	# two
+	let b = 3 + -2
+	let c = a + b
+	let d = [5, 6]
+	let e = b
+	let _ = add(4, 5)
+	#Mod.f()
+}
+`,
+`
+type Person = { name: string, age: int }
+
+type IntList = list int
+
+type Result 'a 'b =
+	| OK 'a
+	| Error 'b
+
+type Option 'a =
+	| Some 'a
+	| None
+
+let main = func {
+	# thing
+	let a = 3
+	let b = 45
+	let c = 5
+	# no
+	let d = [5, 6]
+	let e = b
+	test((5 + 6), Person{name: "no", age: -1})
+}
+
+let test = func p {
+	let c = 5 + 5
+	# mm
+	let a = Person{name:"Josh", age: 32}
+	let b = OK("yes")
+	let c = Error("failed to do thing")
+	let d = Some("braid")
+	let e = None()
+	# hi
+}
+
+`}
+
+func TestExample0(t *testing.T){
+
+	result, err := Compile(examples[0])
+	if err != nil {
+		t.Error(err.Error())
+	}
+	println(result)
+
+}
+
+func TestExample1(t *testing.T){
+
+	result, err := Compile(examples[1])
+	if err != nil {
+		t.Error(err.Error())
+	}
+	println(result)
+
+}
+
+//func TestExample2(t *testing.T){
+//
+//	result, err := Compile(examples[2])
+//	if err != nil {
+//		t.Error(err.Error())
+//	}
+//	println(result)
+//
+//}
