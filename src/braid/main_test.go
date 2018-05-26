0bd6d12d81d0c9d9034af5a6160cf45441c2d076
Josh Sharp <josh@joshsharp.com.au>
2018-05-26 20:05:12 +1000

    update tests to match new syntax

--- src/braid/main_test.go
+++ src/braid/main_test.go
@@ -1,79 +1,80 @@
 package main
 
 import (
-	"testing"
 	"braid/ast"
+	"testing"
 )
 
 var examples = []string{`
 module Main
 
-# test
-let main = func {
+// test
+let main = {
 	5 + 5
 }
 `, `
 module Main
 
-let add = func a b {
+let add = (a, b) {
 	a + b
 }
 
-let main = func {
-	# one
+let main = {
+	// one
 	let a = 2
-	# two
+	// two
 	let b = 3 + -2
 	let c = a + b
 	let d = [5, 6]
 	let e = b
 	let _ = add(4, 5)
-	#Mod.f()
 }
 `,
-`
+	`
 module Main
 
-type Person = { name: string, age: int }
+type Person = { name: string, age: int64 }
 
-type IntList = list int
-
-type Result 'a 'b =
+type Result ('a, 'b) =
 	| OK 'a
 	| Error 'b
 
-type Option 'a =
+type Option ('a) =
 	| Some 'a
 	| None
 
-let main = func {
-	# thing
-	let a = 3
-	let b = 45
-	let c = 5
-	# no
-	let d = [5, 6]
-	let e = b
-	test((5 + 6), Person{name: "no", age: -1})
-}
-
-let test = func p {
-	let c = 5 + 5
-	# mm
-	let a = Person{name:"Josh", age: 32}
+let test = (p: Person) -> () {
+	// comment
+	let a = p
 	let b = OK("yes")
 	let c = Error("failed to do thing")
 	let d = Some("braid")
 	let e = None()
-	# hi
+	let f = a.name
+	let g = 5 + 5
+	// hi
 }
+
+let main = {
+	// thing
+	let a = 3
+	let b = 45
+	let c = 5 + b
+	// another comment
+	let d = [5, 6]
+	let e = b
+	test(Person{name: "no", age: -1})
+}
+
 
 `}
 
-func TestEmptyModule(t *testing.T){
-	m := ast.Module{Name:"Nothing",Subvalues:[]ast.Ast{}}
-	env := make(ast.State)
-	m2, err := ast.Infer(m, &env,[]ast.Type{})
+func TestEmptyModule(t *testing.T) {
+	m := ast.Module{Name: "Nothing", Subvalues: []ast.Ast{}}
+	env := ast.State{Env: make(map[string]ast.Type), UsedVariables: make(map[string]bool),
+		Module: &m,
+	}
+	m2, err := m.Infer(&env, []ast.Type{})
 	if err != nil {
 		t.Error(err.Error())
 	} else {
@@ -84,13 +85,15 @@ func TestEmptyModule(t *testing.T){
 
 }
 
-func TestBasicFunc(t *testing.T){
-	num := ast.BasicAst{ValueType:ast.INT, IntValue:5}
+func TestBasicFunc(t *testing.T) {
+	num := ast.BasicAst{ValueType: ast.INT, IntValue: 5}
 	//e := ast.Expr{Subvalues:[]ast.Ast{num}}
-	f := ast.Func{Name:"Main", Subvalues:[]ast.Ast{num}}
-	m := ast.Module{Name:"Nothing",Subvalues:[]ast.Ast{f}}
-	env := make(ast.State)
-	m2, err := ast.Infer(m, &env,[]ast.Type{})
+	f := ast.Func{Name: "Main", Subvalues: []ast.Ast{num}}
+	m := ast.Module{Name: "Nothing", Subvalues: []ast.Ast{f}}
+	env := ast.State{Env: make(map[string]ast.Type), UsedVariables: make(map[string]bool),
+		Module: &m,
+	}
+	m2, err := m.Infer(&env, []ast.Type{})
 	if err != nil {
 		t.Error(err.Error())
 	} else {
@@ -101,9 +104,9 @@ func TestBasicFunc(t *testing.T){
 
 }
 
-func TestExample0(t *testing.T){
+func TestExample0(t *testing.T) {
 
-	_, err := Compile(examples[0])
+	_, err := Compile(examples[0], false)
 	if err != nil {
 		t.Error(err.Error())
 	}
@@ -111,9 +114,9 @@ func TestExample0(t *testing.T){
 
 }
 
-func TestExample1(t *testing.T){
+func TestExample1(t *testing.T) {
 
-	_, err := Compile(examples[1])
+	_, err := Compile(examples[1], false)
 	if err != nil {
 		t.Error(err.Error())
 	}
@@ -121,12 +124,12 @@ func TestExample1(t *testing.T){
 
 }
 
-//func TestExample2(t *testing.T){
-//
-//	result, err := Compile(examples[2])
-//	if err != nil {
-//		t.Error(err.Error())
-//	}
-//	println(result)
-//
-//}
+func TestExample2(t *testing.T) {
+
+	_, err := Compile(examples[2], false)
+	if err != nil {
+		t.Error(err.Error())
+	}
+	//println(result)
+
+}
